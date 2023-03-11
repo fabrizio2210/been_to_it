e1c75b1a0bdb9b675c8ee7d3d0ad83443e4fac68
Fabrizio W <fabrizio2210@gamil.com>
2023-03-11 16:41:54 +0000

    Adapting Docker files for testing

--- docker/lib/createLocalStack.sh
+++ docker/lib/createLocalStack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Supposing to deploy on x86_64 architecture
-docker build -t fabrizio2210/photobook-backend -f docker/x86_64/Dockerfile-backend .
-docker build -t fabrizio2210/photobook-frontend -f docker/x86_64/Dockerfile-frontend .
-docker-compose -f docker/lib/stack.yml  up
+docker build -t fabrizio2210/been_to_it-backend -f docker/x86_64/Dockerfile-backend .
+docker build -t fabrizio2210/been_to_it-frontend -f docker/x86_64/Dockerfile-frontend .
+docker compose -f docker/lib/stack.yml --env-file="/home/fabrizio/.docker/been_to_it-dev.env" up
