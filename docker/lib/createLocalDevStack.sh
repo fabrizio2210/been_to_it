20dc47c951b45d4e92ba7d3ff94cfd518b29fbcb
Fabrizio W <fabrizio2210@gamil.com>
2023-03-17 08:47:06 +0000

    Added worker to dev stack

--- docker/lib/createLocalDevStack.sh
+++ docker/lib/createLocalDevStack.sh
@@ -3,4 +3,5 @@
 # Supposing to deploy on x86_64 architecture
 docker build -t fabrizio2210/been_to_it-backend-dev -f docker/x86_64/Dockerfile-backend-dev .
 docker build -t fabrizio2210/been_to_it-frontend-dev -f docker/x86_64/Dockerfile-frontend-dev .
+docker build -t fabrizio2210/been_to_it-worker-dev -f docker/x86_64/Dockerfile-worker-dev .
 docker compose -f docker/lib/stack-dev.yml --env-file="/home/fabrizio/.docker/been_to_it-dev.env" up
