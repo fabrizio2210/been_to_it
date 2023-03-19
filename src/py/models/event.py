de07970f38c27b502e245661246485088e203479
Fabrizio W <fabrizio2210@gamil.com>
2023-03-19 12:00:05 +0000

    Use the json function from redisrow and update cells

--- src/py/models/event.py
+++ src/py/models/event.py
@@ -7,19 +7,13 @@ class EventModel(RedisModel):
   
   def __init__(self):
     super().__init__(
-      description_cells=(('A', 3), ('G', '3')),
-      value_cells=(('A', 4), ('G', '4'))
+      description_cells=(('A', 3), ('D', '3')),
+      value_cells=(('A', 4), ('D', '5'))
       )
-
-  def json(self):
-    return {
-        'descrizione_par1': self.descrizione_par1,
-        'descrizione_par2': self.descrizione_par2,
-        'descrizione_par3': self.descrizione_par3,
-      }
 
   @classmethod
   def getEvent(cls):
     if cls.event is None:
       cls.event = EventModel()
     return cls.event
+
