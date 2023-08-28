c89c2c4170db1b440ea344dccdbe3495920aa303
Fabrizio W <fabrizio2210@gamil.com>
2023-08-28 18:49:49 +0100

    Prettier

--- src/vue/src/views/TimelineView.vue
+++ src/vue/src/views/TimelineView.vue
@@ -6,20 +6,24 @@ const { evento } = storeToRefs(useEventStore());
 
 <template>
   <div class="timeline">
-    <img 
-      id=timeline
+    <img
+      id="timeline"
       class="poster"
       src="@/assets/timeline.png"
       alt="15.30 cerimonia, 17:00 aperitvo & fingerfood, 19:00 cena grigliata, 22:00 taglio torta, 22:30 brindisi, 22:45 first dance, 24:00 spaghettata, 24:30 party"
     />
-    <img 
-      v-if="typeof evento !== 'undefined' && evento !== null && typeof evento.allergie !== 'undefined' && itToBool(evento.allergie)"
-      id=menu-cena
+    <img
+      v-if="
+        typeof evento !== 'undefined' &&
+        evento !== null &&
+        typeof evento.allergie !== 'undefined' &&
+        itToBool(evento.allergie)
+      "
+      id="menu-cena"
       class="poster"
       src="@/assets/cena_menu.png"
       alt="BARBEQUE ALLA PINZONERKELLER, Sparerips di maiale, Coscette di pollo marinate al curry e thimo, Salsicce fatte in casa, Roastbeef grigliato tutto d'un pezzo e poi tranciato davanti agli ospiti, Formaggio alla griglia (mini Tomini), Verdure mediterranee grigliate, patate al rosmarino, Polenta cremosa da paiuolo, salsettine fatte in casa (barbeque, salsa verde, maionaise al rafano,…), Bevande per la cena, Vino bianco Sauvignon Fallwind cantina Appiano, Vino rosso Ripasso Valpolicella, Birra alla spina, Acqua, Succhi e softdrinks, Un giro di caffè ed amari/grappe a fine pasto"
     />
-
   </div>
 </template>
 
@@ -33,9 +37,9 @@ export default {
       } else {
         return false;
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 <style>
 .poster {
