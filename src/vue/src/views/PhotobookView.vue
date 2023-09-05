32553fd32ab7079e87c1b263317dc4fd322c99c6
Fabrizio W <fabrizio2210@gamil.com>
2023-09-05 10:56:24 +0100

    Added reference to the photobook

--- src/vue/src/views/PhotobookView.vue
+++ src/vue/src/views/PhotobookView.vue
@@ -1,10 +1,33 @@
-<script setup></script>
+<script setup>
+import { useIdStore } from "../stores/id";
+import { storeToRefs } from "pinia";
+
+const { id } = storeToRefs(useIdStore());
+
+</script>
 
 <template>
   <div class="photobook">
-    <h2>Da qui potrai caricare e vedere le foto durante il matrimonio.</h2>
+    <h2>Carica e vedi le foto durante il matrimonio su <a v-bind:href="url" >
+    i.ve.been.to.it</a></h2>
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    url() {
+      const {id} = storeToRefs(useIdStore());
+      var url = "http://i.ve.been.to.it";
+      if (typeof id !== 'undefined' && id.value !== null) {
+        url = url + "?id=" + id.value;
+      }
+      return url;
+    }
+  }
+}
+
+</script>
 
 <style>
 .photobook {
