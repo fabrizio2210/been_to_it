dcb5dc15245e635b5969140dac126bf08a27ba8e
Fabrizio W <fabrizio2210@gamil.com>
2024-01-11 13:24:18 +0100

    Removal of timeline and menu

--- src/vue/src/views/MenuView.vue
+++ src/vue/src/views/MenuView.vue
@@ -6,42 +6,7 @@ const { evento } = storeToRefs(useEventStore());
 
 <template>
   <div class="menu">
-    <img
-      v-if="
-        typeof evento !== 'undefined' &&
-        evento !== null &&
-        typeof evento.allergie !== 'undefined' &&
-        itToBool(evento.allergie)
-      "
-      id="menu-aperitivo"
-      class="poster"
-      src="@/assets/aperitivo_menu.png"
-      alt="menu aperitivo"
-    />
-    <img
-      v-if="
-        typeof evento !== 'undefined' &&
-        evento !== null &&
-        typeof evento.allergie !== 'undefined' &&
-        itToBool(evento.allergie)
-      "
-      id="menu-cena"
-      class="poster"
-      src="@/assets/cena_menu.png"
-      alt="BARBEQUE ALLA PINZONERKELLER, Sparerips di maiale, Coscette di pollo marinate al curry e thimo, Salsicce fatte in casa, Roastbeef grigliato tutto d'un pezzo e poi tranciato davanti agli ospiti, Formaggio alla griglia (mini Tomini), Verdure mediterranee grigliate, patate al rosmarino, Polenta cremosa da paiuolo, salsettine fatte in casa (barbeque, salsa verde, maionaise al rafano,…), Bevande per la cena, Vino bianco Sauvignon, Vino rosso Ripasso Valpolicella, Birra alla spina, Acqua, Succhi e softdrinks, Un giro di caffè ed amari/grappe a fine pasto"
-    />
-    <img
-      v-if="
-        typeof evento !== 'undefined' &&
-        evento !== null &&
-        typeof evento.allergie !== 'undefined' &&
-        itToBool(evento.allergie)
-      "
-      id="menu-notte"
-      class="poster"
-      src="@/assets/notte_menu.png"
-      alt="menu notte"
-    />
+  <h3>Il menù verrà mostrato qui</h3>
   </div>
 </template>
 
--- src/vue/src/views/TimelineView.vue
+++ src/vue/src/views/TimelineView.vue
@@ -6,13 +6,7 @@ const { evento } = storeToRefs(useEventStore());
 
 <template>
   <div class="timeline">
-    <img
-      id="timeline"
-      v-if="typeof evento !== 'undefined' && evento !== null && evento.id == 0"
-      class="poster"
-      src="@/assets/timeline.png"
-      alt="15.30 cerimonia, 17:00 aperitvo & fingerfood, 19:00 cena grigliata, 22:00 taglio torta, 22:30 brindisi, 22:45 first dance, 24:00 spaghettata, 24:30 party"
-    />
+  <h3>La timeline verrà mostrata qui.</h3>
   </div>
 </template>
 
